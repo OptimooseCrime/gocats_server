ac3c73db82ef111e9c19d89588d8a4c3e0fd905c
Adam Basila <adamcabasila@gmail.com>
2019-05-22 16:11:12 -0600

    add string variables

--- src/gocats.go
+++ src/gocats.go
@@ -8,20 +8,26 @@ import (
 
 func index_handler(w http.ResponseWriter, r *http.Request){
 	//Fprint formats based on what you specified
-	fmt.Fprint(w, "GO GO GO GO Rangers!")
+	fmt.Fprint(w, "<h1>GO GO GO GO Rangers!</h1>")
+	fmt.Fprint(w, "<p>GO HERE, Go There</p>")
+	fmt.Fprint(w, "<p>You can go anywhere!</p>")
+	fmt.Fprint(w, "<p>This is %s passing a %s</p>","me","variable")
+
 }
 
 func about_handler(w http.ResponseWriter, r *http.Request){
-	fmt.Fprint(w, "Whoa dude.... what's this about page all about")
+	fmt.Fprint(w, "Whoa dude.... what's this page all about", "about")
 }
 
 
 func main(){
+	 var x = "Adam"
+	fmt.Println("my name is %s", x)
 	fmt.Println("Sup Earth, this is my server")
 	//handlers: takes URL and take that path and what fuctiuon corresponds to path
 	http.HandleFunc("/", index_handler)
 	http.HandleFunc("/about", about_handler) //the index
  //the index
-	http.ListenAndServe(":8000", nil)
+	http.ListenAndServe(":8000", nil)//ListenAndServeTPS for https
 
 }
